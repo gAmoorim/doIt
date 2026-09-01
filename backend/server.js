d68cd51b7fae53b69bdd713efe8d80870d19193a
gamoorim <gabrielrock012@gmail.com>
2026-09-01 00:59:18 -0300

    troca de porta

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') })
 
 const app = require('./src/app.js')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3100
 
 app.listen(port, () => {
   console.log(`Server rodando na porta ${port}`)
